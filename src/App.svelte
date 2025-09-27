e9abbde38687277e72d4ef1e53addd79d9b8284c
Zachmb <zachmb2008@gmail.com>
2025-09-27 16:38:18 -0500

    init

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+</script>
+
+<main>
+  <h1>Stealth Mode.</h1>
+
+  <p>We're building the future of AI content creation for both SMBs and creators.</p>
+
+  <p>
+    Check out our <a href="https://www.linkedin.com/company/deoso/" target="_blank" rel="noreferrer">LinkedIn</a>.
+  </p>
+</main>
+
+<style>
+</style>
